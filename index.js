14ab28b453edc67e7807c53df3c18e9ca41e6c32
Yingqi Chen <chanwk18@hotmail.com>
2019-01-26 04:38:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function doToElementsInArray(array, callback){
   array.forEach(callback);
 }
 
-function changeCompletely(index, array) {
+function changeCompletely(element,index, array) {
   array[index] = `${array[index]}s!!!`;
 }
